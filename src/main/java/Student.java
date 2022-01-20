1d4282a137c66ba24c08556130e1017712a185ac
maria <marialatka@gmail.com>
2022-01-20 23:46:23 +0100

    second update

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,9 +1,17 @@
 public class Student {
-
+    // comment
     private String name;
     private int age;
 
     public Student(String name) {
         this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
     }
 }
